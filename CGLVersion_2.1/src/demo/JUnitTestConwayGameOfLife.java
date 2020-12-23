8b88408874e42519734db492a0579595afb1c297
tsaideep9835 <tsaideep98@msitprogram.net>
2020-12-23 10:19:47 +0530

    Third commit

---
+++ CGLVersion_2.1/src/demo/JUnitTestConwayGameOfLife.java
@@ -0,0 +1,14 @@
+package demo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JUnitTestConwayGameOfLife {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
