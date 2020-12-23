5284156f68b9ece74095ce244bdc38f359125249
tsaideep9835 <tsaideep98@msitprogram.net>
2020-12-23 20:33:50 +0530

    Fifth commit

--- CGLVersion_2.1/src/demo/ConwayGameOfLife.java
+++ CGLVersion_2.1/src/demo/ConwayGameOfLife.java
@@ -1,40 +1,9 @@
+package demo;
 import java.util.Scanner;
-
-
-
-/**
- * *****Conways Game Of Life************
- * Initially, there is a board with some cells which may be alive or dead. 
- * Task is to generate the next generation of cells based on the following rules: 
- *     
- * Rule 1: Any live cell with fewer than two live neighbors dies, as if caused by under
- * population. 
- * Rule 2:Any live cell with two or three live neighbors lives on
- * to the next generation. 
- * Rule 3: Any live cell with more than three live  neighbors dies, as if by overpopulation. 
- * Ruele 4: Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
- * 
- * Step 1 => find the no Of Neighbours that are alive; 
- * Step 2 => apply rules of game to get next genration by use the if conditions
- * to check all neighbors exluding it self as given video lecture
- * 
- * ****Rules of Life******** 
- * Lonely cell will die ==> aliveNeighbours < 2 ;
- * Cell will die due to overpopulation ==> aliveNeighbours > 3 
- * A new cell will born ==> if currently dead cell have aliveNeighbours == 3
- * 
- * @author Sridevi. K.
- * @author <Student name and roll number>
- * @version Version1.1
- */
 
 class ConwaysGameOfLife {
 
-    /**
-     * Creates initial genration [borad]
-     * @param n  size of the board n X n
-     * @param l[][]  array of live cells
-     */
+ 
     public boolean[][] createBoard(int n, int l[][]) {
         boolean[][] board=new boolean[n][n];
         for(int i=0;i<l.length;i++){
