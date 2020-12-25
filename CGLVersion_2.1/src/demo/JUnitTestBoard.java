54e08cd9fb70591a01123cf64f2c66693cd975e5
tsaideep9835 <tsaideep98@msitprogram.net>
2020-12-25 08:07:34 +0530

    seventh commit

--- CGLVersion_2.1/src/demo/JUnitTestBoard.java
+++ CGLVersion_2.1/src/demo/JUnitTestBoard.java
@@ -1,18 +1,18 @@
 package demo;
-
 import org.junit.jupiter.api.Assertions;
+
 import org.junit.jupiter.api.Test;
 
-
-
-class JUnitTestBoard {
+public class JUnitTestBoard
+{
 
 	@Test
-	public void test() {
+	public void boardTestOne()
+	{
+		
 		int live[][] = {{1,2}}; 
 		Board b = new Board(3,live);
 	    System.out.println("======TEST ONE EXECUTED=======");
 	    Assertions.assertEquals("...\n..*\n...",b.toString());
 	}
-
 }
